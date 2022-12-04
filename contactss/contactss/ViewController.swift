73a4b3dada57b20c3acf670a5cc1a792f308c33d
gdetosy <tosyjdm@gmail.com>
2022-12-04 14:40:56 +0300

    1

---
+++ contactss/contactss/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  contactss
+//
+//  Created by tosy on 4.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
